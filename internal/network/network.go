25ede58e529ccd0da66d35da773a72b33bb03fde
sean <seanm@titan.io>
2024-03-23 14:40:30 -0400

    Adding networking, with testing, removed network/connections, implemented that inside of the protocols

--- internal/network/network.go
+++ internal/network/network.go
@@ -4,18 +4,31 @@ import (
 	"net"
 
 	"github.com/seanmcadam/ctx"
+	"github.com/seanmcadam/loggy"
 	log "github.com/seanmcadam/loggy"
-	"github.com/seanmcadam/octovpn/interfaces"
+	"github.com/seanmcadam/octovpn/interfaces/layers"
 	"github.com/seanmcadam/octovpn/internal/network/tcp"
 	"github.com/seanmcadam/octovpn/internal/network/udp"
 )
 
-func Client(cx *ctx.Ctx, addr net.Addr) (chan interfaces.LayerInterface, error) {
+func Client(cx *ctx.Ctx, addr net.Addr) (chan layers.LayerInterface, error) {
 	switch addr.Network() {
 	case "tcp":
+		fallthrough
+	case "tcp4":
+		fallthrough
+	case "tcp6":
 		return tcp.Client(cx, addr)
 	case "udp":
-		return udp.Client(cx, addr)
+		fallthrough
+	case "udp4":
+		fallthrough
+	case "udp6":
+		udpAddr, ok := addr.(*net.UDPAddr)
+		if !ok {
+			return nil, loggy.Err("addr is not a UDP address")
+		}
+		return udp.Client(cx, udpAddr)
 	default:
 		log.Panic()
 	}
@@ -23,12 +36,24 @@ func Client(cx *ctx.Ctx, addr net.Addr) (chan interfaces.LayerInterface, error) 
 	return nil, nil
 }
 
-func Server(cx *ctx.Ctx, addr net.Addr) (ch chan interfaces.LayerInterface, err error) {
+func Server(cx *ctx.Ctx, addr net.Addr) (ch chan layers.LayerInterface, err error) {
 	switch addr.Network() {
 	case "tcp":
+		fallthrough
+	case "tcp4":
+		fallthrough
+	case "tcp6":
 		ch, _, err = tcp.Server(cx, addr)
 	case "udp":
-		ch, err = udp.Server(cx, addr)
+		fallthrough
+	case "udp4":
+		fallthrough
+	case "udp6":
+		udpAddr, ok := addr.(*net.UDPAddr)
+		if !ok {
+			return nil, loggy.Err("addr is not a UDP address")
+		}
+		return udp.Client(cx, udpAddr)
 	default:
 		log.Panic()
 	}
--- internal/network/network_test.go
+++ internal/network/network_test.go
@@ -7,25 +7,56 @@ import (
 
 	"github.com/seanmcadam/bufferpool"
 	"github.com/seanmcadam/ctx"
-	"github.com/seanmcadam/octovpn/interfaces"
+	"github.com/seanmcadam/octovpn/interfaces/layers"
 	"github.com/seanmcadam/testlib/netlib"
 )
 
 func TestCompile(t *testing.T) {}
 
-func TestConnection(t *testing.T) {
-	var server, client interfaces.LayerInterface
+func TestLayersConnection(t *testing.T) {
+	var server, client layers.LayerInterface
 
-	b1data := []byte("Hi There Cli")
-	b2data := []byte("Hi There Srv")
+	server, client = layers.CreateLayerPair()
+	connection(t, server, client)
+}
+
+func TestTCPConnection(t *testing.T) {
+	var server, client layers.LayerInterface
+	var err error
+
 	cx := ctx.New()
 	ip := net.IPv4(127, 0, 0, 1)
 	port := int(netlib.GetRandomNetworkPort())
 	addr := &net.TCPAddr{IP: ip, Port: port}
-	p := bufferpool.New()
-	b1 := p.Get().Append(b1data)
-	b2 := p.Get().Append(b2data)
 
+	server, client, err = createPair(t, cx, addr)
+	if err != nil {
+		t.Errorf("Error:%s", err)
+		return
+	}
+	connection(t, server, client)
+
+}
+
+func TestUDPConnection(t *testing.T) {
+	var server, client layers.LayerInterface
+	var err error
+
+	cx := ctx.New()
+	ip := net.IPv4(127, 0, 0, 1)
+	port := int(netlib.GetRandomNetworkPort())
+	addr := &net.UDPAddr{IP: ip, Port: port}
+
+	server, client, err = createPair(t, cx, addr)
+	if err != nil {
+		t.Errorf("Error:%s", err)
+		return
+	}
+	connection(t, server, client)
+
+}
+
+func createPair(t *testing.T, cx *ctx.Ctx, addr net.Addr) (server layers.LayerInterface, client layers.LayerInterface, err error) {
 	serverch, err := Server(cx, addr)
 	if err != nil {
 		t.Errorf("Server Err:%s", err)
@@ -56,8 +87,27 @@ func TestConnection(t *testing.T) {
 		t.Errorf("Client timeout")
 		return
 	}
+	return server, client, err
+}
+
+func connection(t *testing.T, server layers.LayerInterface, client layers.LayerInterface) {
+
+	p := bufferpool.New()
+	b1data := []byte("Hi There Cli")
+	b2data := []byte("Hi There Srv")
+	b1 := p.Get().Append(b1data)
+	b2 := p.Get().Append(b2data)
 
 	server.Send(b1)
 	client.Send(b2)
 
+	select {
+	case <-server.RecvCh():
+	case <-client.RecvCh():
+	case <-time.After(time.Second):
+		t.Errorf("Timeout on connection")
+		return
+	}
+
+	t.Logf("connection test completed")
 }
